529c9abd0ec6a413af2b4ac134341b5c5f2a3498
DuyChinh <doanduychinh2102@gmail.com>
2024-12-27 15:47:16 +0700

    add test

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.json({ limit: "50kb" })); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
